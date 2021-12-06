ef11038ad76d9b7f51ea5deaa6ab6d1c8068b5ab
Manikandan Velar <manikan14918@mastek.com>
2021-12-06 04:49:18 +0000

    Updated karma.conf.js

--- angular-frontend/karma.conf.js
+++ angular-frontend/karma.conf.js
@@ -20,6 +20,9 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    proxies: {
+      '/assets/': '/base/src/assets/'
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
