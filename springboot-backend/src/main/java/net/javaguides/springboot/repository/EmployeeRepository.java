45be2e49718388479a200bf2a9f453819f6d1cc0
Pranali Sutar <pranali14932@mastek.com>
2021-12-13 12:23:16 +0530

    commit

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import net.javaguides.springboot.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
+	List<Employee> getEmployeeById(Long id);
+
+
+	
 }
--- springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -3,11 +3,11 @@ package net.javaguides.springboot;
 
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-
-
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,16 +30,16 @@ import net.javaguides.springboot.repository.EmployeeRepository;
 @SpringBootTest
 class SpringbootBackendApplicationTests {
 
-
-
-@Test
-void contextLoads() {
-}
-
-
-
 @Autowired
 private EmployeeController service;
+@Test
+void contextLoads() {
+	assertThat(service).isNull();
+}
+@Test
+void contextLoads2() {
+	assertThat(service).isNotNull();
+}
 
 @MockBean
 private EmployeeRepository repository;
@@ -60,20 +60,21 @@ assertEquals(2,service.getAllEmployees().size());
 }
 
 
-// @Test
-// public void getUserbyAddressTest() {
-// String address = "Bangalore";
-// when(repository.findByAddress(address))
-// .thenReturn(Stream.of(new User(376, "Danile", 31, "USA")).collect(Collectors.toList()));
-// assertEquals(1, service.getUserbyAddress(address).size());
-// }
-//
-// @Test
-// public void saveUserTest() {
-// User user = new User(999, "Pranya", 33, "Pune");
-// when(repository.save(user)).thenReturn(user);
-// assertEquals(user, service.addUser(user));
-// }
+ 
+@Test
+ public void getEmployeeById() {
+ Long id = (long) 1;
+ ((Stream<Employee>) when(repository.getEmployeeById(id))
+ .thenReturn((List<Employee>) Stream.of(new Employee(1,"Pranali","Sutar","Pranali14932@mastek.com","7028971909","Kolhapur","Current")))).collect(Collectors.toList());
+ assertEquals(1, ((List<Employee>) service.getEmployeeById(id)).size());
+ }
+
+@Test
+public void createEmployee() {
+    Employee e1 = new Employee(1,"Pranali","Sutar","Pranali14932@mastek.com","7028971909","Kolhapur","Current");
+when(repository.save(e1)).thenReturn(e1);
+assertEquals(e1, service.createEmployee(e1));
+}
 //
 // @Test
 // public void deleteUserTest() {
