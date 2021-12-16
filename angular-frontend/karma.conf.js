c5a2de9926369a2acc7985e8368895240cca89aa
Pranali Sutar <pranali14932@mastek.com>
2021-12-16 18:26:59 +0530

    update

--- angular-frontend/karma.conf.js
+++ angular-frontend/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
         },
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, './coverage/angular-frontend'),
-            reports: ['html', 'lcovonly', 'text-summary'],
+            reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
             fixWebpackSourcePaths: true
         },
         // proxies: {
