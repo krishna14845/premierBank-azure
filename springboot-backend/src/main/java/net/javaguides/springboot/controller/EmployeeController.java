ccbbd117a3e0788bd5d6ea1cc8d444761bbf2ee2
Manikandan Velar <Manikandan.Velar@mastek.com>
2022-01-31 12:50:40 +0000

    Updated EmployeeController.java

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = {"http://web-app-frontend.azurewebsites.net"
+@CrossOrigin(origins = {"https://webapp-frontend.azurewebsites.net"
 				// ,"https://web-app-frontend-slot.azurewebsites.net"
 	})
 
