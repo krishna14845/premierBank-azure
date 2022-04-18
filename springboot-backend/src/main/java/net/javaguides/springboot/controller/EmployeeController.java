13c90790ec460ded6910fc85a721e790efd0dee9
Pranali Sutar <pranali14932@mastek.com>
2022-04-18 10:46:01 +0000

    Updated EmployeeController.java

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = {"http://webapp-frontend.azurewebsites.net"
+@CrossOrigin(origins = {"http://webapp-frontend.azurewebsites.net","http://20.120.72.61/"
 				// ,"https://web-app-frontend-slot.azurewebsites.net"
 	})
 
