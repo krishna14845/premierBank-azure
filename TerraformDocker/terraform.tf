640158f89f7a87a6b9126a9acbed413e2b96b019
Manikandan Velar <manikan14918@mastek.com>
2022-04-25 10:15:22 +0000

    Updated terraform.tf

--- TerraformDocker/terraform.tf
+++ TerraformDocker/terraform.tf
@@ -23,35 +23,35 @@ terraform {
 }
 
 resource "azurerm_resource_group" "aks-rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "__rgTerraform__"
+  location = "__location__"
 }
 
 resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
+  scope                            = "${azurerm_container_registry.acr.id}"
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  principal_id                     = "${azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id}"
   skip_service_principal_aad_check = true
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  location            = var.location
+  name                = "__acr-name__"
+  resource_group_name = "${azurerm_resource_group.aks-rg.name}"
+  location            = "${azurerm_resource_group.aks-rg.location}"
   sku                 = "Standard"
   admin_enabled       = false
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  location            = var.location
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  dns_prefix          = var.cluster_name
+  name                = "__cluster-name__"
+  kubernetes_version  = "__kubernetes-version__"
+  location            = "${azurerm_resource_group.aks-rg.location}"
+  resource_group_name = "${azurerm_resource_group.aks-rg.name}"
+  dns_prefix          = "${azurerm_resource_group.aks.name}"
 
   default_node_pool {
     name                = "system"
-    node_count          = var.system_node_count
+    node_count          = "__system-node-count__"
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
     availability_zones  = [1, 2, 3]
