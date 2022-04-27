1facf6a7b1ded27053b7b1f268f381f0d6816993
Sagar Jadhav <41576475+sagar89jadhav@users.noreply.github.com>
2022-04-27 13:38:21 +0530

    update frontend endpoint in cross origin

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = {"http://webapp-frontend.azurewebsites.net","http://52.154.249.206"
+@CrossOrigin(origins = {"http://webapp-frontend.azurewebsites.net","http://52.154.249.206", "https://team-sagarjadhav-steelthreading-docker.zeet-team-sagarjadhav.zeet.app/"
 				// ,"https://web-app-frontend-slot.azurewebsites.net"
 	})
 
